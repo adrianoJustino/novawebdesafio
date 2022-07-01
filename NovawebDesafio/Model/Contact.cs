028abfa8b1aa892706c8505e46123c55147c1694
Doom <blacklands00@gmail.com>
2022-07-01 20:19:58 -0300

    Criando classes e controladores

---
+++ NovawebDesafio/Model/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NovawebDesafio.Model
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<string> PhoneNumber { get; set; }
+    }
+}
---
+++ NovawebDesafio/Model/ContactContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NovawebDesafio.Model
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Contact> Contactcs { get; set; }
+    }
+}
